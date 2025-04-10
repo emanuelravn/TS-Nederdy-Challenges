e042bb25111baa285237c0399f95e49f824834ed
emanuelravn <emanuelvalencia@ravn.co>
2025-04-10 13:44:59 -0500

    feat(app): temperature summary challenge completed

--- src/app.ts
+++ src/app.ts
@@ -13,14 +13,42 @@ interface TemperatureSummary {
   average: number
 }
 
+const readingsList: TemperatureReading[] = []
+
 export function processReadings(readings: TemperatureReading[]): void {
-  // add here your code
+  readingsList.push(...readings)
 }
 
 export function getTemperatureSummary(
   date: Date,
   city: string,
 ): TemperatureSummary | null {
-  //add here your code
-  return null
+  const temperatures = readingsList
+    .filter(
+      (reading) =>
+        reading.city === city &&
+        reading.time.toDateString() === date.toDateString(),
+    )
+    .map((reading) => reading.temperature)
+
+  if (temperatures.length === 0) return null
+
+  const firstTemperature = temperatures[0]
+  const lastTemperature = temperatures[temperatures.length - 1]
+  const maxTemperature = Math.max(...temperatures)
+  const minTemperature = Math.min(...temperatures)
+  const averageTemp =
+    temperatures.reduce(
+      (previousTemperature, currentTemperature) =>
+        previousTemperature + currentTemperature,
+      0,
+    ) / temperatures.length
+
+  return {
+    first: firstTemperature,
+    last: lastTemperature,
+    high: maxTemperature,
+    low: minTemperature,
+    average: averageTemp,
+  }
 }
